d98e063dd2877bc8f3e9abc7b66938733572d409
ZEESHAN KHAN <165677660+zeeshankhanT@users.noreply.github.com>
2025-03-06 03:04:45 +0300

    Update Bronze Layer.sql

--- scripts/Bronze Layer.sql
+++ scripts/Bronze Layer.sql
@@ -73,7 +73,7 @@ GO
 TRUNCATE TABLE bronze.crm_cust_info;
 
 BULK INSERT bronze.crm_cust_info
-FROM 'D:\ALL REPORTS\datasets\source_crm\cust_info.csv'
+FROM 'D:\ALL REPORTS\1. Zeeshan\1. Youtube\sql Warehousing\Sql-Warehousing-Project\datasets\source_crm\cust_info.csv'
 WITH (
  FIRSTROW = 2,
  FIELDTERMINATOR = ',',
@@ -82,3 +82,60 @@ WITH (
 
 SELECT * FROM bronze.crm_cust_info;
 SELECT count(*) FROM bronze.crm_cust_info;
+
+TRUNCATE TABLE bronze.crm_prd_info;
+
+BULK INSERT bronze.crm_prd_info
+FROM 'D:\ALL REPORTS\1. Zeeshan\1. Youtube\sql Warehousing\Sql-Warehousing-Project\datasets\source_crm\prd_info.csv'
+WITH (
+ FIRSTROW = 2,
+ FIELDTERMINATOR = ',',
+ TABLOCK --LOCKS THE TABLE WHILE LOADING IT
+);
+
+SELECT * FROM bronze.crm_prd_info;
+SELECT count(*) FROM bronze.crm_prd_info;
+
+TRUNCATE TABLE bronze.crm_sales_details;
+BULK INSERT bronze.crm_sales_details
+FROM 'D:\ALL REPORTS\1. Zeeshan\1. Youtube\sql Warehousing\Sql-Warehousing-Project\datasets\source_crm\sales_details.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+)
+
+SELECT COUNT(*) FROM bronze.crm_sales_details;
+
+TRUNCATE TABLE bronze.erp_cust_aZ12;
+BULK INSERT bronze.erp_cust_aZ12
+FROM 'D:\ALL REPORTS\1. Zeeshan\1. Youtube\sql Warehousing\Sql-Warehousing-Project\datasets\source_erp\CUST_AZ12.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+)
+
+SELECT COUNT(*) FROM bronze.erp_cust_aZ12;
+
+TRUNCATE TABLE bronze.erp_loc_a101;
+BULK INSERT bronze.erp_loc_a101
+FROM 'D:\ALL REPORTS\1. Zeeshan\1. Youtube\sql Warehousing\Sql-Warehousing-Project\datasets\source_erp\LOC_A101.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+)
+
+SELECT COUNT(*) FROM bronze.erp_loc_a101;
+
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'D:\ALL REPORTS\1. Zeeshan\1. Youtube\sql Warehousing\Sql-Warehousing-Project\datasets\source_erp\PX_CAT_G1V2.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+)
+
+SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2;
